0e76863aa6d0682f7ad0fa15bd45bdcd6b526837
flat-lw <leuflatworm@outlook.jp>
2025-03-31 01:16:24 +0900

    v0.5.4

--- Editor/FlatCopyPhysboneComponents.cs
+++ Editor/FlatCopyPhysboneComponents.cs
@@ -99,6 +99,7 @@ namespace Flat.subtools{
             foreach(CopyObjectPair physbonePair in physbonePairs){
                 if(physbonePair.target != null){
                     VRCPhysBone pb = physbonePair.target.AddComponent<VRCPhysBone>();
+                    pb.integrationType = physbonePair.source.GetComponent<VRCPhysBone>().integrationType;
                     pb.rootTransform = physbonePair.source.GetComponent<VRCPhysBone>().rootTransform;
                     List<Transform> ignoreTransforms = physbonePair.source.GetComponent<VRCPhysBone>().ignoreTransforms;
                     if(ignoreTransforms != null){
@@ -106,16 +107,25 @@ namespace Flat.subtools{
                     }
                     pb.endpointPosition = physbonePair.source.GetComponent<VRCPhysBone>().endpointPosition;
                     pb.multiChildType = physbonePair.source.GetComponent<VRCPhysBone>().multiChildType;
-                    pb.integrationType = physbonePair.source.GetComponent<VRCPhysBone>().integrationType;
                     pb.pull = physbonePair.source.GetComponent<VRCPhysBone>().pull;
+                    pb.pullCurve = physbonePair.source.GetComponent<VRCPhysBone>().pullCurve;
                     pb.spring = physbonePair.source.GetComponent<VRCPhysBone>().spring;
+                    pb.springCurve = physbonePair.source.GetComponent<VRCPhysBone>().springCurve;
+                    pb.stiffness = physbonePair.source.GetComponent<VRCPhysBone>().stiffness;
+                    pb.stiffnessCurve = physbonePair.source.GetComponent<VRCPhysBone>().stiffnessCurve;
                     pb.gravity = physbonePair.source.GetComponent<VRCPhysBone>().gravity;
                     pb.gravityFalloff = physbonePair.source.GetComponent<VRCPhysBone>().gravityFalloff;
+                    pb.gravityFalloffCurve = physbonePair.source.GetComponent<VRCPhysBone>().gravityFalloffCurve;
+
                     pb.immobileType = physbonePair.source.GetComponent<VRCPhysBone>().immobileType;
                     pb.immobile = physbonePair.source.GetComponent<VRCPhysBone>().immobile;
-                    pb.limitType = physbonePair.source.GetComponent<VRCPhysBone>().limitType;
-                    pb.radius = physbonePair.source.GetComponent<VRCPhysBone>().radius;
+                    pb.immobileCurve = physbonePair.source.GetComponent<VRCPhysBone>().immobileCurve;
+
                     pb.allowCollision = physbonePair.source.GetComponent<VRCPhysBone>().allowCollision;
+                    pb.collisionFilter.allowSelf = physbonePair.source.GetComponent<VRCPhysBone>().collisionFilter.allowSelf;
+                    pb.collisionFilter.allowOthers = physbonePair.source.GetComponent<VRCPhysBone>().collisionFilter.allowOthers;
+                    pb.radius = physbonePair.source.GetComponent<VRCPhysBone>().radius;
+                    pb.radiusCurve = physbonePair.source.GetComponent<VRCPhysBone>().radiusCurve;
                     List<VRCPhysBoneColliderBase> colliders = physbonePair.source.GetComponent<VRCPhysBone>().colliders;
                     List<VRCPhysBoneColliderBase> targetColliders = new List<VRCPhysBoneColliderBase>();
                     foreach(VRCPhysBoneColliderBase collider in colliders){
@@ -124,16 +134,39 @@ namespace Flat.subtools{
                     if(targetColliders != null){
                         pb.colliders = targetColliders;
                     }
-                    pb.stretchMotion = physbonePair.source.GetComponent<VRCPhysBone>().stretchMotion;
-                    pb.maxStretch = physbonePair.source.GetComponent<VRCPhysBone>().maxStretch;
-                    pb.maxSquish = physbonePair.source.GetComponent<VRCPhysBone>().maxSquish;
+
+                    pb.limitType = physbonePair.source.GetComponent<VRCPhysBone>().limitType;
+                    pb.maxAngleX = physbonePair.source.GetComponent<VRCPhysBone>().maxAngleX;
+                    pb.maxAngleXCurve = physbonePair.source.GetComponent<VRCPhysBone>().maxAngleXCurve;
+                    pb.maxAngleZ = physbonePair.source.GetComponent<VRCPhysBone>().maxAngleZ;
+                    pb.maxAngleZCurve = physbonePair.source.GetComponent<VRCPhysBone>().maxAngleZCurve;
+                    pb.limitRotation = physbonePair.source.GetComponent<VRCPhysBone>().limitRotation;
+                    pb.limitRotationXCurve = physbonePair.source.GetComponent<VRCPhysBone>().limitRotationXCurve;
+                    pb.limitRotationYCurve = physbonePair.source.GetComponent<VRCPhysBone>().limitRotationYCurve;
+                    pb.limitRotationZCurve = physbonePair.source.GetComponent<VRCPhysBone>().limitRotationZCurve;
+
                     pb.allowGrabbing = physbonePair.source.GetComponent<VRCPhysBone>().allowGrabbing;
+                    pb.grabFilter.allowSelf = physbonePair.source.GetComponent<VRCPhysBone>().grabFilter.allowSelf;
+                    pb.grabFilter.allowOthers = physbonePair.source.GetComponent<VRCPhysBone>().grabFilter.allowOthers;
+
                     pb.allowPosing = physbonePair.source.GetComponent<VRCPhysBone>().allowPosing;
-                    pb.grabMovement = physbonePair.source.GetComponent<VRCPhysBone>().grabMovement;
+                    pb.poseFilter.allowSelf = physbonePair.source.GetComponent<VRCPhysBone>().poseFilter.allowSelf;
+                    pb.poseFilter.allowOthers = physbonePair.source.GetComponent<VRCPhysBone>().poseFilter.allowOthers;
+
                     pb.snapToHand = physbonePair.source.GetComponent<VRCPhysBone>().snapToHand;
-                    pb.parameter = physbonePair.source.GetComponent<VRCPhysBone>().parameter;
+                    pb.grabMovement = physbonePair.source.GetComponent<VRCPhysBone>().grabMovement;
+                    pb.maxStretch = physbonePair.source.GetComponent<VRCPhysBone>().maxStretch;
+                    pb.maxStretchCurve = physbonePair.source.GetComponent<VRCPhysBone>().maxStretchCurve;
+                    pb.maxSquish = physbonePair.source.GetComponent<VRCPhysBone>().maxSquish;
+                    pb.maxSquishCurve = physbonePair.source.GetComponent<VRCPhysBone>().maxSquishCurve;
+                    pb.stretchMotion = physbonePair.source.GetComponent<VRCPhysBone>().stretchMotion;
+                    pb.stretchMotionCurve = physbonePair.source.GetComponent<VRCPhysBone>().stretchMotionCurve;
                     pb.isAnimated = physbonePair.source.GetComponent<VRCPhysBone>().isAnimated;
                     pb.resetWhenDisabled = physbonePair.source.GetComponent<VRCPhysBone>().resetWhenDisabled;
+                    pb.parameter = physbonePair.source.GetComponent<VRCPhysBone>().parameter;
+                    pb.showGizmos = physbonePair.source.GetComponent<VRCPhysBone>().showGizmos;
+                    pb.boneOpacity = physbonePair.source.GetComponent<VRCPhysBone>().boneOpacity;
+                    pb.limitOpacity = physbonePair.source.GetComponent<VRCPhysBone>().limitOpacity;
                 }
             }
         }
@@ -145,6 +178,8 @@ namespace Flat.subtools{
                         go.name = colliderPair.source.name;
                         go.transform.parent = colliderPair.target.transform;
                         go.transform.localPosition = colliderPair.source.transform.localPosition;
+                        go.transform.localRotation = colliderPair.source.transform.localRotation;
+                        go.transform.localScale = colliderPair.source.transform.localScale;
                         VRCPhysBoneCollider pbc = go.AddComponent<VRCPhysBoneCollider>();
                         pbc.rootTransform = colliderPair.source.GetComponent<VRCPhysBoneCollider>().rootTransform;
                         pbc.shapeType = colliderPair.source.GetComponent<VRCPhysBoneCollider>().shapeType;
